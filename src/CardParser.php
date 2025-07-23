859a04a6050063236871fd41a0cf3e62fa3b27ee
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2025-07-23 11:30:37 +0100

    🧹 Tidy: CardParser now has the proper error handling, some code tidying and FileHelper implementation

--- src/CardParser.php
+++ src/CardParser.php
@@ -1,61 +1,55 @@
 <?php
 namespace App;
 
-use Error;
+use App\Utilities\FileHelper;
+use RuntimeException;
 
 class CardParser
 {
     public $file;
 
-    public static function getFile(string $file): bool|Error|string
+    public static function parseCardsFromFile(string $file): string|array
     {
-        try {
-            return file_get_contents($file);
-        } catch (Error $error) {
-            return $error;
-        }
+        $content = FileHelper::getFileContent($file);
+        $lines = explode(PHP_EOL, trim($content));
 
-    }
-
-    public static function parseCardsFromFile(string $file): array|string|Error
-    {
         $result = [];
 
-        try {
-            $content = CardParser::getFile($file);
+        foreach ($lines as $line) {
 
-            $lines = explode(PHP_EOL, trim($content));
-
-            foreach($lines as $line) {
-
-                list($card, $numbers) = explode(':', $line);
-
-                // Get card number
-                preg_match('/Card\s+(\d+)/', $line, $matches);
-
-                $card = (int) $matches[1];
-            
-                // split the numbers further into winning and yours
-                list($winning, $yours) = explode('|', $numbers);
-
-                // Need to trim and split numbers into ["39", "54"] etc
-                $winning = array_map('intval', preg_split('/\s+/', trim($winning)));
-                $yours =  array_map('intval', preg_split('/\s+/', trim($yours)));
-                
-                array_push($result, [
-                    'card' => $card,
-                    'winning' => $winning,
-                    'yours' => $yours
-                ]);
+            // Ensure line format is valid
+            if (!str_contains($line, ':') || !str_contains($line, '|')) {
+                throw new RuntimeException("Invalid card format: $line");
+            }
+            // Card number
+            if (!preg_match('/Card\s+(\d+)/', $line, $matches)) {
+                throw new RuntimeException("Missing card number in: $line");
             }
 
-            file_put_contents('files/output/output.json', json_encode($result, JSON_PRETTY_PRINT));
+            list($card, $numbers) = explode(':', $line);
 
-            return 'SUCCESS: Data successfully outputted to files/output/output.json';
-            
-        } catch (Error $error) {
-            return $error;
+            // Get card number
+            preg_match('/Card\s+(\d+)/', $line, $matches);
+
+            $card = (int) $matches[1];
+
+            // split the numbers further into winning and yours
+            list($winning, $yours) = explode('|', $numbers);
+
+            // Need to trim and split numbers into ["39", "54"] etc
+            $winning = array_map('intval', preg_split('/\s+/', trim($winning)));
+            $yours = array_map('intval', preg_split('/\s+/', trim($yours)));
+
+            array_push($result, [
+                'card' => $card,
+                'winning' => $winning,
+                'yours' => $yours
+            ]);
         }
+
+        file_put_contents('files/output/output.json', json_encode($result, JSON_PRETTY_PRINT));
+
+        return 'SUCCESS: Data successfully outputted to files/output/output.json';
 
     }
 }
