dfdf89e8a35914b5cc70c6c4ccf1624055aba2b5
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2025-07-23 10:39:24 +0100

    🔨 CardParser correctly parsing and outputting JSON

--- src/CardParser.php
+++ src/CardParser.php
@@ -17,11 +17,42 @@ class CardParser
 
     }
 
-    public static function parseCardsFromFile(string $file): bool|Error|string
+    public static function parseCardsFromFile(string $file): array|string|Error
     {
+        $result = [];
+
         try {
-            $contentJSON = json_encode(CardParser::getFile($file));
-            return $contentJSON;
+            $content = CardParser::getFile($file);
+
+            $lines = explode(PHP_EOL, trim($content));
+
+            foreach($lines as $line) {
+
+                list($card, $numbers) = explode(':', $line);
+
+                // Get card number
+                preg_match('/Card\s+(\d+)/', $line, $matches);
+
+                $card = (int) $matches[1];
+            
+                // split the numbers further into winning and yours
+                list($winning, $yours) = explode('|', $numbers);
+
+                // Need to trim and split numbers into ["39", "54"] etc
+                $winning = array_map('intval', preg_split('/\s+/', trim($winning)));
+                $yours =  array_map('intval', preg_split('/\s+/', trim($yours)));
+                
+                array_push($result, [
+                    'Card' => $card,
+                    'Winning' => $winning,
+                    'Yours' => $yours
+                ]);
+            }
+
+            file_put_contents('files/output/output.json', json_encode($result, JSON_PRETTY_PRINT));
+
+            return 'SUCCESS: Data successfully outputted to files/output/output.json';
+            
         } catch (Error $error) {
             return $error;
         }
